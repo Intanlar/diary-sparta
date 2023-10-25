3a4cc34305d04395251a8152ef8407b9e6b122a5
Intan Larasati Anisa Rahma <intanlar2002@gmail.com>
2023-10-25 13:55:22 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from datetime import datetime
+
+# Buat koneksi ke basis data MongoDB
+client = MongoClient('mongodb+srv://intanlar:intan@cluster0.cucheap.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta  # Pilih basis data 'dbsparta'
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.diary.find({},{'_id':False}))
+    return jsonify({'articles': articles})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    title_receive = request.form["title_give"]
+    content_receive = request.form["content_give"]
+
+    today = datetime.now()
+    mytime = today.strftime('%d-%m-%Y-%H-%M-%S')
+
+    file = request.files["file_give"]
+    extension = file.filename.split('.')[-1]
+    filename = f'file-{mytime}.{extension}'
+    save_to = f'static/{filename}'
+    file.save(save_to)
+
+    profile = request.files['profile_give']
+    extension = profile.filename.split('.')[-1]  # Perhatikan penggunaan profile.filename
+    profilename = f'profile-{mytime}.{extension}'
+    
+    saveto = f'static/{profilename}'
+    profile.save(saveto)
+
+    time = today.strftime('%d/%m/%Y')
+    
+    doc = {
+        'file': filename,
+        'profile': profilename,
+        'title': title_receive,
+        'content': content_receive,
+        'time' : time,
+    }
+    db.diary.insert_one(doc)
+
+    return jsonify({'msg': 'Data saved successfully!'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
